251e7d738bcded7b69f2846ba9d55d135de9e143
user <email>
2024-04-11 11:34:31 +0200

    Month model added.

--- src/n1/ex1/common/App.java
+++ src/n1/ex1/common/App.java
@@ -1,5 +1,17 @@
 package n1.ex1.common;
 
+/**
+ *
+ * Crea una classe anomenada Month amb un atribut "name" (que emmagatzemarà el nom del mes de l'any).
+ * Afegeix 11 objectes Month (cadascun amb el seu atribut diferent) en un ArrayList, a excepció de l'objecte amb atribut "Agost".
+ * Després, efectua la inserció en el lloc que correspon a aquest mes i demostra que l’ArrayList manté l'ordre correcte.
+ *
+ * Converteix l’ArrayList de l’exercici anterior en un HashSet i assegura’t que no permet duplicats.
+ *
+ * Recorre la llista amb un for i amb un iterador.
+ *
+ */
+
 class App{
     public static void run(){
 
