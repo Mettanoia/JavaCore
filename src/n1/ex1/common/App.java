aeb646f3a11981e007a42bde6d4a571c5f549bbb
user <email>
2024-04-11 11:33:05 +0200

    App module added. Main module added.

---
+++ src/n1/ex1/common/App.java
@@ -0,0 +1,9 @@
+package n1.ex1.common;
+
+class App{
+    public static void run(){
+
+        // TODO
+
+    }
+}
---
+++ src/n1/ex1/common/Main.java
@@ -0,0 +1,7 @@
+package n1.ex1.common;
+
+class Main {
+    public static void main(String[] args) {
+        App.run();
+    }
+}
