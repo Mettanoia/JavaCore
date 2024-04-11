e6f6cf836f3703f5fd34c261b047756498ad237c
user <email>
2024-04-11 12:34:21 +0200

    Level 1 Exercise 2 1.0

--- src/n1/ex2/common/App.java
+++ src/n1/ex2/common/App.java
@@ -1,5 +1,8 @@
 package n1.ex2.common;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * Crea i emplena un List<Integer>.
@@ -13,7 +16,37 @@ package n1.ex2.common;
 public class App {
     public static void run(){
 
+        var ints = new ArrayList<Integer>(){{
+            add(1);
+            add(2);
+            add(3);
+            add(4);
+            add(5);
+            add(6);
+            add(7);
+            add(8);
+            add(9);
+            add(10);
+        }};
 
+        var moreInts = new ArrayList<Integer>(){{
+            add(11);
+            add(12);
+            add(13);
+            add(14);
+            add(15);
+            add(16);
+            add(17);
+            add(18);
+            add(19);
+            add(20);
+        }};
+
+        for (var i = ints.reversed().iterator(); i.hasNext();){
+            moreInts.add(i.next());
+        }
+
+        System.out.println(moreInts.toString());
 
     }
 }
